4f1f444032e89f568e0704d82e1d3c86b1a51ea1
Jorge Sánchez Blanco <jsblanco.dev@gmail.com>
2024-04-21 19:19:35 +0200

    Started project & set up Clerk bases

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import { PropsWithChildren } from 'react';
+
+const AuthLayout = ({ children }: PropsWithChildren) => {
+	return (
+		<div className='flex items-center justify-center h-full pb-16'>
+			{children}
+		</div>
+	);
+};
+
+export default AuthLayout;
---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<main>
+			<h1 className='text-3xl'>Root (protected)</h1>
+		</main>
+	);
+}
