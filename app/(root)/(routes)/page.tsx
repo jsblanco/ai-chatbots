1003125ca46dbbee3f064b5c6893cfc2c526bafb
Jorge Sánchez Blanco <jsblanco.dev@gmail.com>
2024-05-03 23:50:52 +0200

    Created companion cards

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,16 +1,42 @@
 import prismadb from '@/lib/prismadb';
 
 import Categories from '@/components/categories';
+import Companions from '@/components/companions';
 import SearchInput from '@/components/search-input';
 
-export default async function Home() {
+interface RootPageProps {
+	searchParams: {
+		categoryId: string;
+		name: string;
+	};
+}
+
+export default async function Home({ searchParams }: RootPageProps) {
+	const data = await prismadb.companion.findMany({
+		where: {
+			categoryId: searchParams.categoryId,
+			name: {
+				search: searchParams.name,
+			},
+		},
+		orderBy: {
+			createdAt: 'desc',
+		},
+		include: {
+			_count: {
+				select: {
+					messages: true,
+				},
+			},
+		},
+	});
 	const categories = await prismadb.category.findMany();
 
 	return (
 		<main className="h-full p-4 space-y-2">
-			<h1 className="text-3xl">Root (protected)</h1>
 			<SearchInput />
 			<Categories data={categories} />
+			<Companions data={data} />
 		</main>
 	);
 }
